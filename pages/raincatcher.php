16316fb1ac99cef3f7a708b94c6e39795d7dfea1
Oliver Tessmer <oliver.tessmer@gmail.com>
2024-04-08 09:27:37 -0400

    update raincatcher

--- pages/raincatcher.php
+++ pages/raincatcher.php
@@ -1,7 +1,7 @@
 
 <canvas id="gameCanvas">
 </canvas>
-<script src="js/raincatcher.js?v=20240407"></script>
+<script src="js/raincatcher.js?v=20240408"></script>
 
 <a id="source-link" href="https://github.com/tessmero/idle">source</a>
 
